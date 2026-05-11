2265cef7c8aafad78bff4b269ac8fd3cf6633a8d
MACB <milber.ch@gmail.com>
2026-05-11 16:33:08 -0500

    add sql scripto to create the schema

---
+++ design/create_db_schema.sql
@@ -0,0 +1,36 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema macb_dw
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `macb_dw` ;
+
+-- -----------------------------------------------------
+-- Schema macb_dw
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `macb_dw` DEFAULT CHARACTER SET utf8 ;
+USE `macb_dw` ;
+
+-- -----------------------------------------------------
+-- Table `macb_dw`.`usuarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `macb_dw`.`usuarios` (
+  `id_usuario` INT NOT NULL AUTO_INCREMENT,
+  `cedula` VARCHAR(10) NOT NULL,
+  `nombre` VARCHAR(100) NOT NULL,
+  `correo` VARCHAR(100) NOT NULL,
+  `fecha_registro` DATETIME NOT NULL,
+  `password` VARCHAR(20) NOT NULL,
+  UNIQUE INDEX `cedula_UNIQUE` (`cedula` ASC) VISIBLE,
+  UNIQUE INDEX `correo_UNIQUE` (`correo` ASC) VISIBLE,
+  PRIMARY KEY (`id_usuario`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
