a106fff208ee0f2c480c13b8715d4d571310b20c
MACB <milber.ch@gmail.com>
2026-05-12 18:17:10 -0500

    modify db desing

--- design/create_db_schema.sql
+++ design/create_db_schema.sql
@@ -1,5 +1,9 @@
 -- MySQL Workbench Forward Engineering
 
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
 -- -----------------------------------------------------
 -- Schema macb_dw
 -- -----------------------------------------------------
@@ -8,7 +12,7 @@ DROP SCHEMA IF EXISTS `macb_dw` ;
 -- -----------------------------------------------------
 -- Schema macb_dw
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `macb_dw` DEFAULT CHARACTER SET utf8mb4 ;
+CREATE SCHEMA IF NOT EXISTS `macb_dw` DEFAULT CHARACTER SET UTF8MB4 ;
 USE `macb_dw` ;
 
 -- -----------------------------------------------------
@@ -20,19 +24,13 @@ CREATE TABLE IF NOT EXISTS `macb_dw`.`usuarios` (
   `nombre` VARCHAR(100) NOT NULL,
   `correo` VARCHAR(100) NOT NULL,
   `fecha_registro` DATETIME NOT NULL,
-  `password` VARCHAR(20) NOT NULL,
+  `clave_segura` VARCHAR(20) NOT NULL,
   UNIQUE INDEX `cedula_UNIQUE` (`cedula` ASC) VISIBLE,
   UNIQUE INDEX `correo_UNIQUE` (`correo` ASC) VISIBLE,
   PRIMARY KEY (`id_usuario`))
 ENGINE = InnoDB;
 
--- User for app
-CREATE USER IF NOT EXISTS 'macb_app'@'localhost'
-IDENTIFIED BY 'MacbApp2026!';
 
--- Permisos sobre la base
-GRANT ALL PRIVILEGES
-ON macb_dw.*
-TO 'macb_app'@'localhost';
-
-FLUSH PRIVILEGES;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
